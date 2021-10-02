bd6e6d6244b6786e458c6471fbee60bf1dad1ba4
Martín De Angelis <martu@commitstudio.com>
2021-10-02 16:29:02 -0300

    Refactor fetch data an Message component
    - Refactor fetch data
    - Create Message component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,20 +5,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { getVideoSearch } from "../redux/actions";
 import Player from "./player/Player";
 import VideoCard from "./VideoCard";
+import Message from "./Message";
 
 const Main = () => {
   const [searchVideo, setSearchVideo] = useState("");
   // const [searchResults, setSearchResults] = useState([]);
-  const videos = useSelector((state) => state.videos);
-  let selectedVideoId = "";
-  let selectedVideoTitle = "";
-
-  console.log(videos);
-
-  if (videos.length) {
-    selectedVideoId = `https://www.youtube.com/watch?v=${videos[0].id.videoId}`;
-    selectedVideoTitle = videos[0].snippet.title;
-  }
+  const video = useSelector((state) => state.videos[0]);
+  const videoList = useSelector((state) => state.videos);
+  const loading = useSelector((state) => state.isLoading);
 
   const dispatch = useDispatch();
   const handleChange = (e) => {
@@ -28,7 +22,6 @@ const Main = () => {
     e.preventDefault();
     dispatch(getVideoSearch(searchVideo));
   };
-  console.log(setSearchVideo);
 
   return (
     <Container>
@@ -56,27 +49,30 @@ const Main = () => {
         </Col>
       </form>
       <Row>
-        {!videos.length ? (
+        {video == null ? (
           <Col lg={9}>
-            <h1 className="text-center">Search your video</h1>
+            {loading ? (
+              <Message message="Searching..." />
+            ) : (
+              <Message message="Search your video" />
+            )}
           </Col>
         ) : (
           <Col lg={9}>
             <Player
-              url={`https://www.youtube.com/watch?v=${selectedVideoId}`}
+              url={`https://www.youtube.com/watch?v=${video.id.videoId}`}
             />
             <Row>
               <Col md={10} className="video-player-title p-3">
-                <h1>{selectedVideoTitle}</h1>
+                <h1>{video.snippet.title}</h1>
               </Col>
               <Col md={2} className="p-3">
-                {/* <Button variant="secondary" className="w-100 ">
-                  Info
-                </Button> */}
-                <div className="btn secondary">
-                  <Link to="/info">Info</Link>
-                </div>
-                {""}
+                <Link to="/info">
+                  <Button variant="secondary" className="w-100">
+                    Info
+                  </Button>
+                  {""}
+                </Link>
               </Col>
             </Row>
           </Col>
@@ -84,7 +80,7 @@ const Main = () => {
 
         <Col lg={3}>
           <div className="related-videos">
-            {videos.splice(1, 3).map((video, i) => (
+            {videoList.splice(1, 3).map((video, i) => (
               <VideoCard
                 key={i}
                 title={video.snippet.title}
---
+++ src/components/Message.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Message({ message }) {
+  return (
+    <>
+      <h1 className="text-center">{message}</h1>
+    </>
+  );
+}
+
+export default Message;
